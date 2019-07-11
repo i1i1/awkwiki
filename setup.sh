2def88aa9e273a3ddf3aea25a7f3481973e309cc
Ivan <vanyarybin1@live.ru>
2019-07-11 22:20:45 +0300

    Changed setup script

--- setup.sh
+++ setup.sh
@@ -4,7 +4,18 @@ dst_files="cgi-bin/awkpasswd.dst cgi-bin/awki.conf.dst resources/site_logo.png.d
 
 for file in $dst_files ; do
 	dst_name=`echo $file | sed 's/.dst$//'`
-	echo "[+] Moving $file into $dst_name"
-	mv $file $dst_name
+	echo "[+] Copying $file into $dst_name"
+	cp $file $dst_name
 done
+
+if awk -F= '/^ID_LIKE=/ {exit($2 == "debian" ? 0 : 1)}'; then
+	echo "[+] Installing dependencies"
+	sudo apt-get install gawk groff ps2eps pdf2svg python3-pygments
+	cd /tmp
+	wget http://mirrors.ctan.org/support/epstopdf.zip
+	unzip epstopdf.zip
+	sudo mv epstopdf/epstopdf.pl /usr/bin/epstopdf
+else
+	echo "[+] Skipping installation of dependencies"
+fi
 
